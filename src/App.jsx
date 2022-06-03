5d6d225590cbc58000e5ade2b95af33554ef04d8
Hayden Gray <35206453+A1029384756@users.noreply.github.com>
2022-06-03 14:50:19 -0400

    Fixed updating

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,21 @@ import Selector from './UpdateSelector/selector'
 import { useState } from 'react'
 import { useEffect } from 'react'
 import { queryDatabase } from './utils/firebase'
-import { Transport } from './utils/webserial'
-import { ESPLoader } from './utils/ESPLoader'
+import { connect } from './utils/esptools/index'
+import { sleep } from './utils/esptools/util'
 
-let device = null
-let transport
-let chip = 'default'
-let esploader
-let connected = false
+let espStub
+const baudRate = 115200
+const bufferSize = 512
+const measurementPeriodId = '0001'
+
 let firmwareArr = []
 
 function App() {
   const [firmwareList, updateList] = useState(null)
   const [selectedFirmware, updateSelectedFirmware] = useState(null)
+  const [connected, toggleConnected] = useState(false)
+  const [updating, toggleUpdating] = useState(false)
 
   const handleChange = (newSelection) => {
     updateSelectedFirmware(newSelection)
@@ -30,38 +32,48 @@ function App() {
     var zip = require('jszip')()
     zip = await zip.loadAsync(blob)
     for (let filename in zip.files) {
-      firmwareArr.push(zip.files[filename].async('text'))
+      firmwareArr.push(zip.files[filename].async('blob'))
     }
   }
 
   const portScan = async () => {
-    if (device === null) {
-      device = await navigator.serial.requestPort({
-        // filters: [{usbVendorId: 0x1a86}]
-      })
-      transport = new Transport(device)
-      console.log(transport)
+
+    if (espStub) {
+      await espStub.disconnect()
+      await espStub.port.close()
+      espStub = undefined
+      toggleConnected(!connected)
+      return
     }
-    
+
+    const esploader = await connect({
+      log: (...args) => console.log(...args),
+      debug: (...args) => console.debug(...args),
+      error: (...args) => console.error(...args)
+    })
+
     try {
-      esploader = new ESPLoader(transport, 115200)
-      connected = true
-      chip = await esploader.main_fn()
+      await esploader.initialize()
+
+      console.log("Connected to " + esploader.chipName)
       
-      await esploader.flash_id()
-    } catch(e) {
-      console.log(e)
+      espStub = await esploader.runStub()
+      toggleConnected(!connected)
+    } catch (err) {
+      await esploader.disconnect()
+      throw err
     }
   }
   
   const updateOrbit = async () => {
     if ((selectedFirmware !== null && selectedFirmware !== 'invalid') && connected) {
+      toggleUpdating(!updating)
       downloadFirmware().then(() => {
         console.log('download and extraction complete')
-        Promise.all(firmwareArr).then((data) => {
+        Promise.all(firmwareArr).then(async (data) => {
           let parsedFirmware = []
           for (let element in data) {
-            if (data[element] !== '') {
+            if (data[element].size !== 0) {
               parsedFirmware.push(data[element])
             }
           }
@@ -75,19 +87,47 @@ function App() {
           const PARTITIONS_OFFSET = 0x8000
           const APP_OFFSET = 0xe000
           const FIRMWARE_OFFSET = 0x10000
+
+          const blobToArrayBuffer = async (blob) => {
+            return await blob.arrayBuffer()
+          }
+
+          for (var i = 0; i < 4; i++) {
+            firmwareArr[i] = await blobToArrayBuffer(firmwareArr[i])
+            console.log(firmwareArr[i])
+          }
   
           fileArr.push({data: firmwareArr[1], address:BOOTLOADER_OFFSET})
           fileArr.push({data: firmwareArr[3], address: PARTITIONS_OFFSET})
           fileArr.push({data: firmwareArr[0], address: APP_OFFSET})
           fileArr.push({data: firmwareArr[2], address: FIRMWARE_OFFSET})
-  
-          esploader.write_flash({fileArray: fileArr, flash_size: 'keep'})
+
+          console.log(fileArr)
+          
+          for (var i = 0; i < 4; i++) {
+            try {
+              await espStub.flashData(
+                fileArr[i].data,
+                (bytesWritten, totalBytes) => {
+                  console.log(100*bytesWritten/totalBytes)
+                },
+                fileArr[i].address
+              )
+              await sleep(100)
+            } catch (err) {
+              console.error(err)
+              toggleUpdating(!updating)
+            }
+          }
+          console.log('Finished')
+          toggleUpdating(false)
         })
       })
+
         
     } else if (!connected) {
-      alert('Please connect to the Orbit!')
-    } else {
+      alert('Please connect to the Orbit')
+    }else {
       alert('Please select a firmware version!')
     }
   }
@@ -107,8 +147,8 @@ function App() {
           <img className='marbl' src={marblImg}/>
           <Selector firmwareList={firmwareList} onChange={handleChange}/>
           <div className='buttonContainer'>
-            <button className='button' onClick={portScan}>Connect</button>
-            <button className='button' onClick={updateOrbit}>Update</button>
+            <button className='button' onClick={portScan}>{connected ? "Connected" : "Connect"}</button>
+            <button className='button' onClick={updateOrbit} disabled={updating}>{updating ? "Updating" : "Update"}</button>
           </div>
           <img className='axibo' src={axiboImg}/>
         </div>
